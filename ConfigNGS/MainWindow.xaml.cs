676dae4885a91e98525b61341f84a56bcdab6a5b
rarida <50086079+rarida@users.noreply.github.com>
2021-04-13 02:18:13 -0400

    Add files via upload

---
+++ ConfigNGS/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using OfficeOpenXml;
+
+namespace ConfigNGS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        //jsonObject jsonObject;
+
+        jsonObject.Root root;
+
+
+
+        private void FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateSpreadSheet();
+
+
+        }
+
+        private void FolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader r = new StreamReader(@"C:\Users\jimja\Desktop\SpreadSheets\new.json"))
+            {
+                string json = r.ReadToEnd();
+
+                //string json = @"{'Email': 'james@example.com','Active': true,'CreatedDate': '2013-01-20T00:00:00Z','Roles': ['User','Admin']}";
+
+                //jo = JsonConvert.DeserializeObject<jsonObject>(json);
+
+                //DateInfo.Text = jo.Email;
+
+                root = JsonConvert.DeserializeObject<jsonObject.Root>(json);
+
+                //Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+                //jsonObject obj = JsonConvert.DeserializeObject<jsonObject>(json);
+
+                DateInfo.Text = root.kits[0].loci.
+
+
+
+
+                //List<jsonObject> items = JsonConvert.DeserializeObject<List<jsonObject>>(json);
+                //jsonObject item = JsonConvert.DeserializeObject<jsonObject>(json);
+
+
+                //DateInfo.Text = item.testString;
+            }
+
+            //string json = @"{'Email': 'james@example.com','Active': true,'CreatedDate': '2013-01-20T00:00:00Z','Roles': ['User','Admin']}";
+
+            //jsonObject jo = JsonConvert.DeserializeObject<jsonObject>(json);
+
+            //DateInfo.Text = jo.Email;
+        }
+        
+
+        void CreateSpreadSheet()
+        {
+            string spreadsheetPath = "C:\\Users\\jimja\\Desktop\\SpreadSheets\\New.xlsx";
+
+            ExcelPackage ExcelPkg = new ExcelPackage();
+            ExcelWorksheet wsSheet1 = ExcelPkg.Workbook.Worksheets.Add("Sheet1");
+            ExcelWorksheet wsSheet2 = ExcelPkg.Workbook.Worksheets.Add("Sheet2");
+
+
+            //wsSheet1.Cells[4, 1].Value = jo.Email;
+
+            ExcelPkg.SaveAs(new FileInfo(spreadsheetPath));
+        }
+
+    }
+}
---
+++ ConfigNGS/jsonObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigNGS
+{
+    namespace jsonObject
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class DefaultStutter
+        {
+            public double PhrThreshold { get; set; }
+            public int NPlus { get; set; }
+            public int NMinus { get; set; }
+        }
+
+        public class DefaultSequencingError
+        {
+            public double IntensityThreshold { get; set; }
+        }
+
+        public class PlusSequence
+        {
+            public string Seq { get; set; }
+        }
+
+        public class MinusSequence
+        {
+            public string Seq { get; set; }
+        }
+
+        public class Flank5p
+        {
+            public string Seq { get; set; }
+        }
+
+        public class Flank3p
+        {
+            public string Seq { get; set; }
+        }
+
+        public class RepeatMotif
+        {
+            public string Seq { get; set; }
+        }
+
+        public class Stutter
+        {
+            public double PhrThreshold { get; set; }
+            public int NPlus { get; set; }
+            public int NMinus { get; set; }
+        }
+
+        public class Loci
+        {
+            public string Marker { get; set; }
+            public PlusSequence PlusSequence { get; set; }
+            public MinusSequence MinusSequence { get; set; }
+            public Flank5p Flank5p { get; set; }
+            public Flank3p Flank3p { get; set; }
+            public List<RepeatMotif> RepeatMotifs { get; set; }
+            public List<Stutter> Stutter { get; set; }
+            public string AlleleOffset { get; set; }
+            public int? SizeStandard { get; set; }
+        }
+
+        public class Kit
+        {
+            public string Name { get; set; }
+            public List<DefaultStutter> DefaultStutter { get; set; }
+            public List<DefaultSequencingError> DefaultSequencingError { get; set; }
+            public List<Loci> loci { get; set; }
+        }
+
+        public class Root
+        {
+            public int AbundanceThresholdInput { get; set; }
+            public int AbundanceThresholdOutput { get; set; }
+            public bool OutputRawSequences { get; set; }
+            public string DefaultKit { get; set; }
+            public int OutputAbundantAlleleSequences { get; set; }
+            public List<Kit> kits { get; set; }
+        }
+
+
+    }
+}
